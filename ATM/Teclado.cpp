ae194a1d35095b98a78e59045e74585890d18a5f
Luis2eed <luis.zitroxhz@gmail.com>
2025-05-16 19:39:29 -0600

    ProyectoFinalPoo
    Final

---
+++ ATM/Teclado.cpp
@@ -0,0 +1,36 @@
+// Teclado.cpp
+#include <iostream>
+#include <conio.h>  // Solo para _getch() en Windows
+#include "Teclado.h"
+
+int Teclado::obtenerEntrada() const 
+{
+    int entrada;
+    std::cin >> entrada;
+    return entrada;
+}
+
+int Teclado::obtenerNIPEnmascarado() const 
+{
+    std::string input;
+    char ch;
+
+    std::cout << "Introduzca su NIP: ";
+
+    while ((ch = _getch()) != '\r') 
+    {  
+        if (ch >= '0' && ch <= '9') 
+        {
+            std::cout << '*';
+            input += ch;
+        }
+        else if (ch == '\b' && !input.empty()) 
+        {  // Backspace
+            std::cout << "\b \b";
+            input.pop_back();
+        }
+    }
+
+    std::cout << std::endl;
+    return input.empty() ? 0 : std::stoi(input);
+}
---
+++ ATM/Teclado.h
@@ -0,0 +1,14 @@
+// Teclado.h
+// Definición de la clase Teclado. Representa el teclado del ATM.
+#ifndef TECLADO_H
+#define TECLADO_H
+class Teclado
+{
+public:
+int obtenerEntrada() const; // devuelve un valor entero introducido por el usuario
+int obtenerNIPEnmascarado() const;
+
+
+}; // fin de la clase Teclado
+
+ #endif // TECLADO_H
